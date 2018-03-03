797343ff1ca5a4502e23dcd2d2e8c4c54e2e5110
Han Xiao <han@hanxbox.com>
2018-03-03 17:41:33 -0500

    ready for lab3 work session

--- Lab/Lab3/LogicalStep_Lab3_top.vhd
+++ Lab/Lab3/LogicalStep_Lab3_top.vhd
@@ -10,11 +10,11 @@ use ieee.numeric_std.all;
 
 
 entity LogicalStep_Lab3_top is port (
-   clkin_50		: in	std_logic;
-	pb				: in	std_logic_vector(3 downto 0);
- 	sw   			: in  std_logic_vector(7 downto 0); -- The switch inputs
-   leds			: out std_logic_vector(7 downto 0);	-- for displaying the switch content
-   seg7_data 	: out std_logic_vector(6 downto 0); -- 7-bit outputs to a 7-segment
+    clkin_50	: in	std_logic;
+    pb			: in	std_logic_vector(3 downto 0);
+ 	sw   		: in    std_logic_vector(7 downto 0);       -- The switch inputs
+    leds		: out   std_logic_vector(7 downto 0);	    -- for displaying the switch content
+    seg7_data 	: out   std_logic_vector(6 downto 0);       -- 7-bit outputs to a 7-segment
 	seg7_char1  : out	std_logic;							-- seg7 digi selectors
 	seg7_char2  : out	std_logic							-- seg7 digi selectors
 	
@@ -22,19 +22,34 @@ entity LogicalStep_Lab3_top is port (
 end LogicalStep_Lab3_top;
 
 architecture Energy_Monitor of LogicalStep_Lab3_top is
---
--- Components Used
-------------------------------------------------------------------- 
 
-------------------------------------------------------------------
+-- Components Used
+
+    component Compx4 port(
+        A       :   in  std_logic_vector(3 downto 0);
+        B       :   in  std_logic_vector(3 downto 0);
+        GT      :   out std_logic;
+        EQ      :   out std_logic;
+        LE      :   out std_logic
+    );
+    end component;
+
+
+
 	
 	
 -- Create any signals, or temporary variables to be used
 	
-	
+	gt      : std_logic;
+	eq      : std_logic;
+	le      : std_logic;
+
+
 -- Here the circuit begins
 
 begin
+
+    Comp4Inst   : Compx4 port map(sw(3 downto 0), sw(7 downto 4), gt, eq, le);
  
 end Energy_Monitor;
 
